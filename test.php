c6cf9ecc3a3d29c7f5bf274a4024a8067e9b9e02
shekharwms <38559544+shekharwms@users.noreply.github.com>
2018-04-20 17:54:54 +0530

    add_file

---
+++ test.php
@@ -0,0 +1,9 @@
+<?php
+if (isset($_COOKIE['cookie'])) {
+    foreach ($_COOKIE['cookie'] as $name => $value) {
+        $name = htmlspecialchars($name);
+        $value = htmlspecialchars($value);
+        echo "$name : $value <br />\n";
+    }
+}
+?>
